2447228b02e1e9a3d98af0e2ad7adde362f04a14
Andrew Rubenstein <andjruben@gmail.com>
2023-04-24 00:24:01 -0400

    feat: add backdrop & loading indicator components

---
+++ src/app/components/Backdrop/index.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Backdrop = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/app/components/LoadingIndicator/index.tsx
@@ -0,0 +1,57 @@
+import styled, { keyframes } from 'styled-components';
+
+type Props = SvgProps
+
+export const LoadingIndicator = (props: Props) => (
+  <Svg viewBox="-24 -24 48 48" small={props.small}>
+    <Circle
+      cx="0"
+      cy="0"
+      r="20"
+      fill="none"
+      strokeWidth="4"
+      stroke={props.stroke}
+    ></Circle>
+  </Svg>
+);
+
+const speed = 1.5;
+
+const rotate = keyframes`
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const dash = keyframes`
+  0% {
+    stroke-dasharray: 0, 150;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 100, 150;
+    stroke-dashoffset: -24;
+  }
+  100% {
+    stroke-dasharray: 0, 150;
+    stroke-dashoffset: -124;
+  }
+`;
+
+interface SvgProps {
+  small?: boolean;
+  stroke?: string;
+}
+
+const Svg = styled.svg<SvgProps>`
+  animation: ${rotate} ${speed * 1.75}s linear infinite;
+  height: ${p => (p.small ? '1.25rem' : '3rem')};
+  width: ${p => (p.small ? '1.25rem' : '3rem')};
+  transform-origin: center;
+`;
+
+const Circle = styled.circle<SvgProps>`
+  animation: ${dash} ${speed}s ease-in-out infinite;
+  stroke: ${p => p.stroke || p.theme.backgroundVariant};
+  stroke-linecap: round;
+`;
