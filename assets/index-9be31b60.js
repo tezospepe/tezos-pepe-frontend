9d14360219f0dd809da3c0322d45ea1a68e69a3a
Qwiddle <andjruben@gmail.com>
2023-04-27 18:37:40 -0400

    Updates

--- assets/index-9be31b60.js
+++ assets/index-9be31b60.js
@@ -1,11 +1,11 @@
-import{s as n,j as t,B as i}from"./index-bbf1d3ce.js";var r=(e=>(e[e.Primary=0]="Primary",e[e.Secondary=1]="Secondary",e[e.Outline=2]="Outline",e))(r||{});const o=n.div`
+import{s as n,j as t,B as i}from"./index-58d6287e.js";var r=(e=>(e[e.Primary=0]="Primary",e[e.Secondary=1]="Secondary",e[e.Outline=2]="Outline",e))(r||{});const a=n.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 1rem;
   padding-top: 2rem;
-`,a=n.h1`
+`,o=n.h1`
   color: ${e=>e.theme.text};
   font-size: 2.5rem;
   text-align: center;
@@ -34,7 +34,7 @@ import{s as n,j as t,B as i}from"./index-bbf1d3ce.js";var r=(e=>(e[e.Primary=0]=
   background-color: 
     ${e=>e.variant==r.Outline?"transparent":e.theme.backgroundVariant};
   cursor: pointer;
-`,d=()=>t.jsxs(o,{children:[t.jsx(a,{children:"PEPE without the Gas Fees"}),t.jsxs(c,{children:[t.jsx("span",{children:"If a picture is worth a thousand words, then a meme is worth a thousand pictures!"}),t.jsx("span",{children:"Tezos Pepe is no different - Pepes may look alike, but no two Pepes are the same! With Tezos Pepe, tezonians can have fun building, trading, playing, memeing, and using the DAO treasury to fund other projects."})]}),t.jsxs(l,{children:[t.jsx("a",{href:"https://pepeswap.tez.page/",children:t.jsx(s,{children:"PepePot"})}),t.jsx("a",{href:"https://pepeswap.tez.page/",children:t.jsx(s,{variant:r.Outline,children:"PepeSwap"})})]})]}),p="/tezos-pepe/assets/pepelegs-b08cbfb7.png",h=n.img`
+`,d=()=>t.jsxs(a,{children:[t.jsx(o,{children:"PEPE without the Gas Fees"}),t.jsxs(c,{children:[t.jsx("span",{children:"If a picture is worth a thousand words, then a meme is worth a thousand pictures!"}),t.jsx("span",{children:"Tezos Pepe is no different - Pepes may look alike, but no two Pepes are the same! With Tezos Pepe, tezonians can have fun building, trading, playing, memeing, and using the DAO treasury to fund other projects."})]}),t.jsxs(l,{children:[t.jsx("a",{href:"https://pepeswap.tez.page/",children:t.jsx(s,{children:"PepePot"})}),t.jsx("a",{href:"https://pepeswap.tez.page/",children:t.jsx(s,{variant:r.Outline,children:"PepeSwap"})})]})]}),p="/assets/pepelegs-b08cbfb7.png",h=n.img`
   width: 100%;
   max-width: 800px;
   height: auto;
