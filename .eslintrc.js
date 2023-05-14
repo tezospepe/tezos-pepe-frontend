cee8f17593a5d61b75b8ecceb5e037ee40e66a3a
Andrew Rubenstein <andjruben@gmail.com>
2023-05-13 23:37:37 -0400

    chore: proper linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
@@ -10,5 +18,12 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': ['error', prettierOptions],
   },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: { 'prettier/prettier': ['warn', prettierOptions] },
+    }
+  ]
 }
